69b609098f5b07dd027b5362d4836ddd18519296
Oksana Ostapuk <oksanaostapuk@gmail.com>
2023-12-14 18:04:43 +0300

    Part2 Task2: Updated reference to Class in index.php.

--- Part2Task2/index.php
+++ Part2Task2/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use Part2Task2\Form;
+use \Part2Task2\Form;
 
 echo (new Form())->open(['action'=>'index.php', 'method'=>'POST']);
 echo (new Form())->input(['type'=>'text', 'placeholder'=>'Ваше имя', 'name'=>'name']);
