41183c6988b31d47c02cd987f93fceffebec8e0a
Oksana Ostapuk <oksanaostapuk@gmail.com>
2023-12-14 15:02:14 +0300

    Part2 Task1: Replaced results to .html file. Added links to result file.

--- TasksP2N1.php
+++ TasksP2N1.php
@@ -1,22 +1,33 @@
 <?php
+ini_set('display_errors', 'Off');
 
 function searchUrl(string $page): bool
 {
-    $resultFile = fopen('result_url.txt', 'w');
+    $resultFile = fopen('result_url.html', 'w');
     if ($resultFile) {
         $doc = new DOMDocument();
         $doc->loadHTMLFile(trim($page));
-
-        //$xpath = new DOMXpath($doc);
-
+        $k1 = stristr(trim($page), '.', true); //ссылка до точки домена
+        $k2 = stristr(trim($page), '.'); //ссылка, начиная с точки в домене
+        $k3 = stristr($k2, '/', true); //домен
+        $domName = $k1 . $k3;
+        //echo $domName . "\n";
         $tagsA = $doc->getElementsByTagName('a');
 
         if (isset($tagsA->length)) {
+            fwrite($resultFile, '<!DOCTYPE html>' . "\n");
+            fwrite($resultFile, '<body>' . "\n");
             for ($i = $tagsA->length; --$i >= 0;) {
                 $tagA = $tagsA->item($i);
                 $href = $tagA->getAttribute('href');
-                fwrite($resultFile, $href . "\n");
+                if ($href !== '#' && $href !== ' ' && $href !== '' && $href !== '/') {
+                    fwrite($resultFile, '<a href="' . $href . '">' . $href . '</a><br>' . "\n");
+                }
+                if ($href[0] === '/') {
+                    fwrite($resultFile, '<a href="' . $domName . $href . '">' . $domName . $href . '</a><br>' . "\n");
+                }
             }
+            fwrite($resultFile, '</body>' . "\n");
             fclose($resultFile);
             return true;
         } else throw new Exception('There are no links on the page.');
@@ -44,6 +55,7 @@ function searchUrl(string $page): bool
         try {
             if(searchUrl($_POST['input_page'])) {
                 echo 'Success!';
+    ?><a href="result_url.html">Посмотреть результат</a> <?php
             } else echo 'Smth is wrong! :(';
         } catch (Exception $e) {
             echo $e->getMessage();
