31a5e719cc3171729ddc61628731dbef08726d27
Oksana Ostapuk <oksanaostapuk@gmail.com>
2023-12-15 12:10:55 +0300

    Part2 Task2: Changed connection to Class via 'include' & adjusted the attribute output method .

--- Part2Task2/Form/Form.php
+++ Part2Task2/Form/Form.php
@@ -1,9 +1,6 @@
 <?php
 
 
-namespace Form;
-
-
 class Form
 {
     private array $params = [];
@@ -16,8 +13,8 @@ class Form
     private function setAttributes(array $attributes): string
     {
         $result = '';
-        foreach ($attributes as $attribute) {
-            $result .= key($attributes) . $attribute;
+        foreach ($attributes as $key=>$value) {
+            $result .= $key . '="' . $value . '" ';
         }
         return $result;
     }
@@ -25,21 +22,19 @@ class Form
     public function input(array $attributes): string
     {
         $text = $this->setAttributes($attributes);
-        return '<input ' . $text . '>';
+        return '<input ' . $text . '><br/>';
     }
 
     public function submit(array $attributes): string
     {
-        array_unshift($attributes, ['type' => 'submit']);
-        $text = $this->setAttributes($attributes);
-        return '<input ' . $text . '>';
+        $text = $this->setAttributes(array_merge(['type' => 'submit'], $attributes));
+        return '<input ' . $text . '><br/>';
     }
 
     public function password(array $attributes): string
     {
-        array_unshift($attributes, ['type' => 'password']);
-        $text = $this->setAttributes($attributes);
-        return '<input ' . $text . '>';
+        $text = $this->setAttributes(array_merge(['type' => 'password'], $attributes));
+        return '<input ' . $text . '><br/>';
     }
 
     public function textarea(array $attributes): string
@@ -48,17 +43,17 @@ class Form
             $value = $attributes['value'];
             unset($attributes['value']);
             $text = $this->setAttributes($attributes);
-            return '<textarea ' . $text . '>' . $value . '</textarea>';
+            return '<textarea ' . $text . '>' . $value . '</textarea><br/>';
         } else {
             $text = $this->setAttributes($attributes);
-            return '<textarea ' . $text . '</textarea>';
+            return '<textarea ' . $text . '></textarea><br/>';
         }
     }
 
     public function open(array $attributes): string
     {
         $text = $this->setAttributes($attributes);
-        return '<form ' . $text . '>';
+        return '<form ' . $text . '><br/>';
     }
 
     public function close(): string
--- Part2Task2/index.php
+++ Part2Task2/index.php
@@ -1,9 +1,11 @@
 <?php
+ini_set('display_errors', 'Off');
 
-use Form\Form;
+include './Form/Form.php';
 
-echo (new Form())->open(['action'=>'index.php', 'method'=>'POST']);
-echo (new Form())->input(['type'=>'text', 'placeholder'=>'Ваше имя', 'name'=>'name']);
-echo (new Form())->password(['placeholder'=>'Ваш пароль', 'name'=>'pass']);
-echo (new Form())->submit(['value'=>'Отправить']);
+echo (new Form())->open(['action' => 'index.php', 'method' => 'POST']);
+echo (new Form())->input(['type' => 'text', 'placeholder' => 'Ваше имя', 'name' => 'name']);
+echo (new Form())->password(['placeholder' => 'Ваш пароль', 'name' => 'pass']);
+echo (new Form())->textarea(['rows' => '10', 'cols' => '45', 'name' => 'text']);
+echo (new Form())->submit(['value' => 'Отправить']);
 echo (new Form())->close();
